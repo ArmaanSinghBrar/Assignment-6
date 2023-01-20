96a369b3c044f3cf007a418fd0fb90d906efb874
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-20 22:38:46 +0530

    Update Question 2.py

--- Question 2.py
+++ Question 2.py
@@ -1,7 +1,8 @@
 #Finding if a string is palindrome
-String=input("Enter a string :")
-Reversed_String=String[::-1]             #Reversing the string
-if String==Reversed_String:              #If string stays same when reversed it is a palindrome
-    print(String,"is a palindrome")
-else:
-    print(String,"is not a palindrome")
+def palindrome(string):
+  String=string.replace(" ","")
+  reversed_string=String[::-1]             #Reversing the string
+  if String==reversed_string:              #If string stays same when reversed it is a palindrome
+    return True
+  else:
+    return False
