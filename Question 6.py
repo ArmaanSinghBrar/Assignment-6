4bb173a853eb2e816b16fa61d7d53e2180929c6b
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-26 11:57:31 +0530

    Create Question 6.py

---
+++ Question 6.py
@@ -0,0 +1,8 @@
+#Student Data Function
+def student_data(student_id, student_name=None, student_class=None):
+    print("Student ID:", student_id)
+    if student_name:
+        print("Student Name:", student_name)
+    if student_class:
+        print("Student Class:", student_class)
+student_data(1)
