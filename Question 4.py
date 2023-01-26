b4b200419760b0bbe2be1582da427b98bd4505f5
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-26 11:44:58 +0530

    Update Question 4.py

--- Question 4.py
+++ Question 4.py
@@ -1,1 +1,11 @@
+#Function to check if a string is a pangram
+def pangram(string):
+    string=string.lower()
+    for i in range(97,123):        #running a loop for all alphabets
+        statement=chr(i) in string   #Statement is true or false
+        if statement==False:         #If statement becomes false at any point the string is not a pangram
+            return False
+    else:
+        return True
 
+print(pangram("The quick brown fox jumps over the lazy dog"))   #An Example
