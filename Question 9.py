68ddd05bfc5b6442fd3a6fcaeabbe267b82c7fe7
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-31 19:06:31 +0530

    Update Question 9.py

--- Question 9.py
+++ Question 9.py
@@ -1,1 +1,14 @@
-
+class ParenthesisChecker:
+    def __init__(self, s):
+        self.s = s
+        
+    def is_valid(self):
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+        for char in self.s:
+            if char in mapping:
+                if not stack or stack.pop() != mapping[char]:
+                    return False
+            else:
+                stack.append(char)
+        return not stack
