7b2eba2a122dd88c05e1ba326c47b270fd4ac544
ArmaanSinghBrar <120672171+ArmaanSinghBrar@users.noreply.github.com>
2023-01-31 19:02:49 +0530

    Update Question 3.py

--- Question 3.py
+++ Question 3.py
@@ -1,1 +1,16 @@
-
+def pascals_triangle(n):
+    result = []
+    for i in range(n):
+        if i == 0:
+            result.append([1])
+        else:
+            row = [1]
+            for j in range(1, i):
+                row.append(result[i-1][j-1] + result[i-1][j])
+            row.append(1)
+            result.append(row)
+    for row in result:
+        for j in range(len(row),len(result)):
+            print(" ",end="")
+        print(row)
+    print("")
